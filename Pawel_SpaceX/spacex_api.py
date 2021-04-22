ee14a9b73d95b570b4d75b23036460fee33095d4
FuRaJ3003 <furajchirashi3003@gmail.com>
2021-04-22 17:30:41 +0200

    spacex_api_init

---
+++ Pawel_SpaceX/spacex_api.py
@@ -0,0 +1,74 @@
+import csv
+import requests
+import json
+
+class RecordsAPI():
+    BASE_URL = r"https://api.spacexdata.com/v3/launches/"
+
+    def __init__(self, flight_number, mission_name, rocket_id, rocket_name, launch_date_utc, video_link):
+        self.flight_number = flight_number
+        self.mission_name = mission_name
+        self.rocket_id = rocket_id
+        self.rocket_name = rocket_name
+        self.launch_date_utc = launch_date_utc
+        self.video_link = video_link
+
+    @classmethod
+    def get_from_api(cls, flight_number):
+        url = cls.BASE_URL + flight_number
+        json_data = requests.get(url).json()
+        return RecordsAPI(
+            flight_number = flight_number,
+            mission_name = json_data.get("mission_name"),
+            rocket_id = json_data.get("rocket"),
+            rocket_name = json_data.get("rocket"),
+            launch_date_utc = json_data.get("launch_date_utc"),
+            video_link = "video_link")
+        
+    @classmethod
+    def get_results(cls):
+        result = requests.get(RecordsAPI.API_KEY)
+        result = result.json()
+        return result
+
+    @classmethod
+    def show_api(cls):
+        print(json.dumps(RecordsAPI.get_results(), sort_keys=True, indent=4))
+
+
+class Flight(RecordsAPI):
+    BASE_URL = r"https://api.spacexdata.com/v3/launches/"
+
+    def __init__(self, flight_number, mission_name, rocket_id, rocket_name, launch_date_utc, video_link):
+        self.flight_number = flight_number
+        self.mission_name = mission_name
+        self.rocket_id = rocket_id
+        self.rocket_name = rocket_name
+        self.launch_date_utc = launch_date_utc
+        self.video_link = video_link
+        
+    @classmethod
+    def create_by_flight_id(cls, flight_number):
+        url = cls.BASE_URL + flight_number
+        json_data = requests.get(url).json()
+        flight = RecordsAPI.get_from_api(flight_number)
+        return Flight(flight_number,
+            json_data["mission_name"],
+            json_data["rocket"]["rocket_id"], 
+            json_data["rocket"]["rocket_name"], 
+            json_data["launch_date_utc"], 
+            json_data["links"]["video_link"])
+
+    def show_info(self):
+            print(f"""
+            ------------ FLIGHT NUMBER {self.flight_number} -------------\n
+            Mission name:\t {self.mission_name}\n
+            Rocket ID: \t\t {self.rocket_id}\n
+            Rocket Name: \t {self.rocket_name}\n
+            Launch Date (UTC): \t {self.launch_date_utc}\n
+            Video Link: \t {self.video_link}\n
+            """)
+
+new_flight = Flight.create_by_flight_id("77")
+new_flight.show_info()
+
